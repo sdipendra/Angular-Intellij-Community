c8d27e8930542a5a2a3c5adbfce675ed03d2e476
GitHub Action <action@github.com>
2024-09-03 23:23:13 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/sdipendra/angularintellijcommunity/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.sdipendra.angularintellijcommunity.services
+
+import com.github.sdipendra.angularintellijcommunity.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/sdipendra/angularintellijcommunity/services/MyProjectService.kt
+++ src/main/kotlin/com/github/sdipendra/angularintellijcommunity/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.sdipendra.angularintellijcommunity.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sdipendra.angularintellijcommunity.MyBundle
 
 class MyProjectService(project: Project) {
 
